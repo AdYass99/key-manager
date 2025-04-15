d9385a24bcb22f0ea24bedc02c9eba8748c8632b
Adnan Yassifi <ayassifi@voltec.es>
2025-04-15 09:25:21 +0200

    .

--- app/routes/passwords.py
+++ app/routes/passwords.py
@@ -11,22 +11,21 @@ def add():
     query_set(f"INSERT INTO users (username, password) VALUES ('{username}', '{password}')")
     return redirect(url_for('passwords.config'))
 
-@passwords_bp.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query', '')
-    data = query_get(f"SELECT id, username, password FROM users WHERE username LIKE '%{query}%'")
-    return render_template('search.html', data=data, query=query)
+@passwords_bp.route('/view', methods=['GET'])
+def view():
+    data = query_get(f"SELECT id, username, password FROM users")
+    return render_template('search.html', data=data)
 
-@passwords_bp.route('/view/<int:user_id>', methods=['GET'])
-def view(user_id):
-    data = query_get(f"SELECT password FROM users WHERE Id LIKE '%{user_id}%'")
+@passwords_bp.route('/view/<string:user_id>', methods=['GET'])
+def search(user_id):
+    data = query_get(f"SELECT id,username, password FROM users WHERE username LIKE '%{user_id}%'")
     return render_template('search.html', data=data)
 
 @passwords_bp.route('/copy/<int:user_id>', methods=['GET'])
 def copy(user_id):
     data = query_get(f"SELECT password FROM users WHERE Id = {user_id}")
     pyperclip.copy(data[0][0])
-    return redirect(url_for('passwords.search'))
+    return redirect(request.referrer)
 
 @passwords_bp.route('/config', methods=['GET'])
 def config():
@@ -38,14 +37,13 @@ def delete(user_id):
     query_set(f"DELETE FROM users WHERE id = {user_id}")
     return redirect(url_for('passwords.config'))
 
-
 @passwords_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         password = request.form['password']
         if password == '1234':
             session['logged_in'] = True
-            return redirect(url_for('passwords.search'))
+            return redirect(url_for('passwords.view'))
         else:
             return render_template('index.html', error="Contraseña incorrecta")
     return render_template('index.html')
