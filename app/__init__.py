02afce718e2e5b0321d0e79f868d2636575ff00f
Adnan Yassifi <ayassifi@voltec.es>
2025-06-06 20:43:58 +0200

    flask talisman2

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ It also sets up the database connection and configuration settings.
 import os
 
 from flask import Flask, session
+from flask_talisman import Talisman
 
 from app.db.database import init_db
 from app.routes.authentication import autentification_bp
@@ -17,10 +18,33 @@ def create_app():
     app = Flask(__name__)
     app.config['DATABASE_PATH'] = os.path.join('instance','passwords.db')
     app.config['SECRET_KEY'] = 'tfg_uoc_key_manager'
+    # Configurar cookies seguras
+    app.config.update(
+        SESSION_COOKIE_SECURE=True,
+        SESSION_COOKIE_HTTPONLY=True,
+        SESSION_COOKIE_SAMESITE='Lax',
+    )
+
+    # Activar protección CSP + HSTS + X-Frame-Options
+    Talisman(
+        app,
+        content_security_policy={
+            'default-src': "'self'",
+            'script-src': "'self'",
+            'connect-src': "'self'",
+            'img-src': "'self' data:",
+            'style-src': "'self'"
+        },
+        force_https=True,
+        frame_options='DENY'
+    )
+
+    # Registrar blueprints
+    
     app.register_blueprint(passwords_bp)
     app.register_blueprint(autentification_bp)
     app.add_url_rule('/', 'home', home)
-    app.secret_key = 'uoc'  # Cambia esto por una clave secreta segura
+    app.secret_key = 'uoc'
 
     @app.context_processor
     def inject_user():
